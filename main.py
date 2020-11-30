d9a924362a58ac181127bfcf532a9f8e07069751
Philipp Mulfinger <info@phil-splash.de>
2020-11-30 17:10:00 +0100

    done the rest of the 16.1 homework

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 from flask import Flask, render_template
+import datetime
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    some_text = "Message from the handler."
+    current_year = datetime.datetime.now().year
 
-@app.route("/about")
+    cities = ["Boston", "Vienna", "Paris", "Berlin"]
+
+    return render_template("index.html", some_text=some_text, current_year=current_year, cities=cities)
+
+@app.route("/about-me")
 def about_me():
     return render_template("about.html")
 
